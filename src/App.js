78977c64edf76c799eb7d0d377693403d1acb4fa
Jadude <160776823+Jadude@users.noreply.github.com>
2024-06-12 16:10:44 +0200

    cleanRepository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                header
+            </header>
+            <section>2</section>
+            <section>3</section>
+            <section>4</section>
+            <footer className="App-footer">
+                footer
+            </footer>
+        </div>
+    );
 }
 
 export default App;
