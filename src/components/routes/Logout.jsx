83821837ed1d941dd62217ea1e0451484b0ccef8
Jadude <160776823+Jadude@users.noreply.github.com>
2024-06-18 23:09:26 +0200

    logout

--- src/components/routes/Logout.jsx
+++ src/components/routes/Logout.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from "../layout/Basic/Header";
+import decoration from '../../assets/icons/Decoration.svg';
 
 const Logout = () => {
     return (
         <>
-            <Header />
-        <h2>Wylogowano Cię</h2>
-    <span>decor</span>
-    <span>załóż konto</span>
+        <Header />
+        <div className="Logout-container">
+            <div className="Logout-box">
+                <h2>Wylogowanie nastąpiło pomyślnie!</h2>
+                <img src={decoration} alt="decoration"/>
+                <a href="/">Strona główna</a>
+            </div>
+        </div>
         </>
     );
 };
